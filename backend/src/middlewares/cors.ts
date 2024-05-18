a36d4d7bc397428d152d94144cf5c1648edd1dd1
Juan Manuel <juanmi_4000@hotmail.es>
2024-05-18 21:40:19 +0200

    Terminado

--- backend/src/middlewares/cors.ts
+++ backend/src/middlewares/cors.ts
@@ -5,12 +5,14 @@ const ORIGINES_ACEPTADOS = [
   'http://localhost:8080'
 ]
 
-export const corsMiddleware = ({ origenesAceptados = ORIGINES_ACEPTADOS } = {}): ReturnType<typeof cors> => cors({ // solucionarlo (mirarlo)
+export const corsMiddleware = ({ origenesAceptados = ORIGINES_ACEPTADOS } = {}): ReturnType<typeof cors> => cors({
   origin: (origen, callback) => {
     if ((origen !== undefined && typeof origen === 'string')) { // cambiarlo
       if (origenesAceptados.includes(origen)) {
         return callback(null, true)
       }
+    } else if (origen === undefined) {
+      return callback(null, true)
     }
     return callback(new Error('Not allowed by CORS'))
   }
