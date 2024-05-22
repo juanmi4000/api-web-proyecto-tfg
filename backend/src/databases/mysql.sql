7cae75cf4e7922ea11d622ae2984106cb8d15125
Juan Manuel <juanmi_4000@hotmail.es>
2024-05-22 11:21:46 +0200

    Base de datos para mysql

--- backend/src/databases/mysql.sql
+++ backend/src/databases/mysql.sql
@@ -7,11 +7,11 @@ use proyectotfg;
 drop table if exists anime;
 drop table if exists genero;
 drop table if exists animes_generos;
-drop table if exists usuarios;
+drop table if exists usuario;
 
 -- creación de tablas
 create table anime (
-	id binary(16) primary key default (UUID_TO_BIN(UUID())),
+	id varchar(36) not null primary key,
     estudio varchar(200) not null,
     descripcion text not null,
     fecha_inicio date not null,
@@ -20,12 +20,30 @@ create table anime (
 );
 
 create table genero (
-	id binary(16) primary key default (UUID_TO_BIN(UUID())),
+	id varchar(36) not null primary key,
     nombre varchar(255) not null unique
 );
 
 create table animes_generos (
-	anime_id binary(16) references animes(id),
-    genero_id binary(16) references generos(id),
+	anime_id varchar(36) not null,
+    genero_id varchar(36) not null,
+    foreign key (anime_id) references anime(id) on delete cascade,
+    foreign key (genero_id) references genero(id) on delete cascade,
     primary key (anime_id, genero_id)
+);
+
+create table usuario (
+	id varchar(36) not null primary key,
+    nombre_pila varchar(100) not null,
+    apellido varchar(100) not null,
+    usuario varchar(100) not null unique,
+    email varchar(100) not null unique,
+    contrasena varchar(100) not null,
+    telefono varchar(100) not null unique,
+    genero varchar(100) not null,
+    calle_nombre varchar(100) not null,
+    calle_numero int not null,
+    img_grande text not null,
+    img_mediana text not null,
+    img_miniatura text not null
 );
