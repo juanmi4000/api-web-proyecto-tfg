19070770b02b36cc150dfe8a7a956f6c8128d35d
Juan Manuel <juanmi_4000@hotmail.es>
2024-05-26 23:37:44 +0200

    Añadido plugin de animaciones y una fuente

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        fugaz: ['Fugaz One', 'system-ui', 'sans-serif']
+      }
+    }
   },
-  plugins: []
+  plugins: [animations]
 }
