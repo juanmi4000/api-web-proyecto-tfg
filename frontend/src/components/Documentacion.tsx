dca56a8969132c07d6566496b71d5eb8dd3ec47a
Juan Manuel <juanmi_4000@hotmail.es>
2024-06-07 00:11:44 +0200

    Realizada página documentación

--- frontend/src/components/Documentacion.tsx
+++ frontend/src/components/Documentacion.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, Fragment } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import docs_info from '@/json/docs-info.json'
 import docs_titulo from '@/json/docs-titulos.json'
 import { MostrarOcultarInfo } from '@comp/BtnMostrar'
@@ -6,6 +6,7 @@ import { AsteriscoReact } from '@/multimedia/iconos/Asterisco'
 import { CopiarReact } from '@/multimedia/iconos/Copiar'
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import '@/estilos/documentacion.css'
 
 interface Contenido {
   titulo: string
@@ -33,6 +34,7 @@ export function DocumentacionComp (): JSX.Element {
       setInfo(contenidoEncontrado.contenido)
     }
   }, [idMostrarInfo])
+
   const esIntroduccion = tituloPrincipal === 'Introducción'
 
   const renderizarDescripcion = (descripcion: string): JSX.Element[] => {
@@ -41,23 +43,24 @@ export function DocumentacionComp (): JSX.Element {
       return (
         <Fragment key={index}>
           {fragmento}
-          {
-            arrDescripcion.length - 1 === index
-              ? <br />
-              : (
-                <>
-                  <br />
-                  <br />
-                </>
-                )
-          }
-
+          {arrDescripcion.length - 1 === index
+            ? (
+              <br />
+              )
+            : (
+              <>
+                <br />
+                <br />
+              </>
+              )}
         </Fragment>
       )
     })
   }
 
-  const copiarPortapapeles = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): Promise<void> => {
+  const copiarPortapapeles = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): Promise<void> => {
     // const textoCopiar = (e.target as HTMLButtonElement).getAttribute('data-copiar') // me interesa el que elemento que originó el evento aunque es el svg en este caso
     const textoCopiar = e.currentTarget.getAttribute('data-copiar')
     if (textoCopiar === null) return
@@ -70,71 +73,130 @@ export function DocumentacionComp (): JSX.Element {
     notify()
   }
 
+  const manejadorSelect = (
+    evento: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
+    const opcion = evento.currentTarget
+    const indice = opcion.selectedIndex
+    const opciones = opcion.options
+    const id = opciones[indice].getAttribute('data-id')
+    const titulo = opcion.value
+    if (id === null) return
+    setIdMostrarInfo(id)
+    setTituloPrincipal(titulo)
+  }
+
   return (
-    <>
-      <header className='hidden pt-10 pl-5 md:flex md:flex-col md:gap-10 md:border-r-2 overflow-y-auto'>
-        {docs_titulo.map(({ id, titulo, subtitulos }) => (
-          <article key={id}>
-            <p className='font-fugaz cursor-default'>{titulo}</p>
-            {subtitulos.map(({ id: idSubtitulo, titulo: subtitulo }) => (
-              <button
-                className='ml-5 text-lg my-1 block'
-                key={idSubtitulo}
-                onClick={() => {
-                  setIdMostrarInfo(idSubtitulo)
-                  setTituloPrincipal(subtitulo)
-                }}
-              >
-                {subtitulo}
-              </button>
-            ))}
-          </article>
-        ))}
-      </header>
-      <main className='pt-10 md:pr-5 lg:border-r-2 lg:pr-10 lg:flex lg: flex-col lg:gap-10 overflow-y-auto'>
-        <h2 className='font-fugaz text-4xl'>{tituloPrincipal}</h2>
-        <hr />
-        {info.map(
-          (
-            { titulo, descripcion, codigoReq, copiar, info, codigoRes, idHref },
-            index
-          ) => (
-            <article className='mb-10' key={index} id={idHref}>
-              {!esIntroduccion && <h3 className='font-fugaz text-2xl'>{titulo}</h3>}
-              <p className='mb-7'>{renderizarDescripcion(descripcion)}</p>
-              {codigoReq !== undefined && (
-                <>
-                  <p className='text-lg mb-7'>Petición:</p>
-                  <code className='relative mb-3 block bg-[rgb(var(--color-fondo-codigo))] p-2 rounded-md w-full text-black'>
-                    <button className='absolute top-2 right-2 border bg-[rgb(var(--color-fondo-copiar))] p-1 rounded' data-copiar={copiar} onClick={(e) => { copiarPortapapeles(e).catch((error) => { console.error(error) }) }}>
-                      <span><CopiarReact clases='w-5' /></span>
-                      <ToastContainer autoClose={2000} />
-                    </button>
-                    <p>{codigoReq}</p>
-                  </code>
-                </>
-              )}
-              {info !== undefined && (
-                <p className='flex justify-start items-center mb-7 text-base'>
-                  <span><AsteriscoReact clases='w-5' /></span>
-                  <span className='ml-3'>{info}</span>
-                </p>
-              )}
-              {codigoRes !== undefined && (
-                <MostrarOcultarInfo codigoRes={codigoRes} />
-              )}
+    <main className='relative px-5' id='ocultar-main'>
+      <section className='absolute text-black h-16 px-6 py-5 border-t-2 w-full top-0 left-0 bg-[rgb(var(--color-header))] md:hidden'>
+        <select
+          className='rounded-md px-2 py-0.5 bg-[rgb(var(--color-fondo-copiar))]'
+          onChange={(e) => {
+            manejadorSelect(e)
+          }}
+        >
+          {docs_titulo.map(({ subtitulos }) => {
+            return subtitulos.map(({ id, titulo }) => {
+              return (
+                <option value={titulo} key={id} data-id={id}>
+                  {titulo}
+                </option>
+              )
+            })
+          })}
+        </select>
+      </section>
+      <section id='docs' className='gap-2 p-1'>
+        <header className='hidden pt-10 pl-5 md:flex md:flex-col md:gap-10 md:border-r-2 overflow-y-auto'>
+          {docs_titulo.map(({ id, titulo, subtitulos }) => (
+            <article key={id}>
+              <p className='font-fugaz cursor-default'>{titulo}</p>
+              {subtitulos.map(({ id: idSubtitulo, titulo: subtitulo }) => (
+                <button
+                  className='ml-5 text-lg my-1 block hover:text-slate-400'
+                  key={idSubtitulo}
+                  onClick={() => {
+                    setIdMostrarInfo(idSubtitulo)
+                    setTituloPrincipal(subtitulo)
+                  }}
+                >
+                  {subtitulo}
+                </button>
+              ))}
             </article>
-          )
-        )}
-      </main>
-      <footer className='hidden pt-10 lg:flex lg:flex-col lg:gap-2 w-full overflow-y-auto'>
-        <h3 className='font-fugaz text-lg'>En esta página</h3>
-        {
-          info.map(({ href, titulo }, index) => (
-            <a href={href} key={index} className='text-base'>{titulo}</a>
-          ))
-        }
-      </footer>
-    </>
+          ))}
+        </header>
+        <main className='pt-10 flex flex-col gap-10 overflow-y-auto md:pr-5 lg:border-r-2 lg:pr-10'>
+          <h2 className='font-fugaz text-3xl md:text-4xl'>{tituloPrincipal}</h2>
+          <hr />
+          {info.map(
+            (
+              {
+                titulo,
+                descripcion,
+                codigoReq,
+                copiar,
+                info,
+                codigoRes,
+                idHref
+              },
+              index
+            ) => (
+              <article className='mb-10' key={index} id={idHref}>
+                {!esIntroduccion && (
+                  <h3 className='font-fugaz text-2xl'>{titulo}</h3>
+                )}
+                <p className='mb-7'>{renderizarDescripcion(descripcion)}</p>
+                {codigoReq !== undefined && (
+                  <>
+                    <p className='text-lg mb-7'>Petición:</p>
+                    <code className='relative mb-3 block bg-[rgb(var(--color-fondo-codigo))] p-0.5 rounded-md w-full text-black'>
+                      <button
+                        className='absolute top-2 right-2 border bg-[rgb(var(--color-fondo-copiar))] p-1 rounded'
+                        data-copiar={copiar}
+                        onClick={(e) => {
+                          copiarPortapapeles(e).catch((error) => {
+                            console.error(error)
+                          })
+                        }}
+                      >
+                        <span>
+                          <CopiarReact clases='w-5' />
+                        </span>
+                        <ToastContainer autoClose={2000} />
+                      </button>
+                      <pre className='overflow-x-auto p-2'>{codigoReq}</pre>
+                    </code>
+                  </>
+                )}
+                {info !== undefined && (
+                  <p className='flex justify-start items-center mb-7 text-base'>
+                    <span>
+                      <AsteriscoReact clases='w-5' />
+                    </span>
+                    <span className='ml-3'>{info}</span>
+                  </p>
+                )}
+                {codigoRes !== undefined && (
+                  <MostrarOcultarInfo codigoRes={JSON.parse(codigoRes)} />
+                )}
+              </article>
+            )
+          )}
+        </main>
+        <footer className='hidden pt-10 lg:flex lg:flex-col lg:gap-2 w-full overflow-y-auto'>
+          <h3 className='font-fugaz text-lg'>En esta página</h3>
+          {info.map(({ href, titulo }, index) => (
+            <a
+              href={href}
+              key={index}
+              className='text-base hover:text-slate-400'
+            >
+              {titulo}
+            </a>
+          ))}
+        </footer>
+      </section>
+    </main>
   )
 }
--- frontend/src/components/btnMostrar.tsx
+++ frontend/src/components/btnMostrar.tsx
@@ -1,7 +1,17 @@
 import { useState } from 'react'
 
+interface Contenido {
+  titulo: string
+  descripcion: string
+  codigoReq?: string
+  codigoRes?: string
+  copiar?: string
+  info?: string
+  idHref: string
+  href: string
+}
 interface Props {
-  codigoRes: string
+  codigoRes: Contenido[]
 }
 
 export function MostrarOcultarInfo ({ codigoRes }: Props): JSX.Element {
@@ -9,6 +19,10 @@ export function MostrarOcultarInfo ({ codigoRes }: Props): JSX.Element {
 
   const manejadorBtn = (): void => {
     setPulsado(!pulsado)
+  }
+
+  const comprobarCodigoRes = (): Contenido | Contenido[] => {
+    return codigoRes.length !== 1 ? codigoRes : codigoRes[0]
   }
 
   return (
@@ -22,8 +36,8 @@ export function MostrarOcultarInfo ({ codigoRes }: Props): JSX.Element {
       {pulsado && (
         <div>
           <p className='text-lg mb-7'>Respuesta:</p>
-          <code className='mb-7 block bg-[rgb(var(--color-fondo-codigo))] p-2 rounded-md w-full text-black'>
-            <p>{codigoRes}</p>
+          <code className='mb-7 block bg-[rgb(var(--color-fondo-codigo))] p-2 rounded-md w-full text-black overflow-auto'>
+            <pre>{JSON.stringify(comprobarCodigoRes(), null, 2)}</pre>
           </code>
         </div>
       )}
