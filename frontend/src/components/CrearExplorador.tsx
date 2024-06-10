c5d5562cbd718743571a025431e71c73bcda97bc
Juan Manuel <juanmi_4000@hotmail.es>
2024-06-10 14:02:06 +0200

    Añadidas funciones para controlar los parámetros que introducen y el fetch

--- frontend/src/components/CrearExplorador.tsx
+++ frontend/src/components/CrearExplorador.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from 'react'
+import { AnadirParamentros } from './AnadirParametros'
+import { SelectOpciones } from './SelectOpciones'
+import { FormularioExplorador } from './FormularioExplorador'
+import type { Anime, Titulo, Usuario, UsuarioCalle, UsuarioImagen, UsuarioNombre, Parametros, Respuesta } from '@/tipos/tipos'
 import '@/estilos/crear-explorador.css'
 
 interface Props {
   children: string
-}
-
-interface Respuesta {
-  [key: string]: any
-}
-
-interface Parametros {
-  key: `${string}-${string}-${string}-${string}-${string}`
 }
 
 export function CrearExplorador ({ children }: Props): JSX.Element {
@@ -21,7 +17,72 @@ export function CrearExplorador ({ children }: Props): JSX.Element {
   const esGet = opcion.toUpperCase() === 'GET'
   const esDetete = opcion.toUpperCase() === 'DELETE'
 
-  const handleForm = async (
+  const primeraLetraMayus = (cadena: string): string => {
+    const arrCadena = cadena.split(' ').map(cad => (cad.charAt(0).toUpperCase() + cad.slice(1).toLowerCase()).trim()).filter((cad) => cad !== '')
+    let cadAux = ''
+    arrCadena.forEach((cad, index) => {
+      if (arrCadena.length - 1 !== index) {
+        cadAux += cad.trim() + ' '
+      } else {
+        cadAux += cad.trim()
+      }
+    })
+    return cadAux
+  }
+
+  const comprobarParametros = (divs: NodeListOf<HTMLDivElement>, value: string): Anime | Usuario | undefined => {
+    const [endpoint] = value.split('/')
+    if (endpoint === 'animes') {
+      const anime: Anime = {}
+      const titulo: Titulo = {}
+
+      divs.forEach((div) => {
+        const inputs = div.querySelectorAll('input')
+        const [generico, diferencial] = inputs[0].value.split('_')
+
+        if (generico === 'titulo' && (diferencial === 'enlace' || diferencial === 'texto')) {
+          titulo[diferencial] = inputs[1].value
+          anime.titulo = titulo
+        } else if (inputs[0].value === 'generos') {
+          // primeraLetraMayus(inputs[1].value).forEach(h => console.log(h))
+          anime.generos = inputs[1].value.split(',').map(genero => primeraLetraMayus(genero))
+        } else {
+          (anime as any)[inputs[0].value] = inputs[1].value
+        }
+      })
+      return anime
+    } else if (endpoint === 'usuarios') {
+      const usuario: Usuario = {}
+      const nombre: UsuarioNombre = {}
+      const calle: UsuarioCalle = {}
+      const imagen: UsuarioImagen = {}
+      divs.forEach(div => {
+        const inputs = div.querySelectorAll('input')
+        const [generico, diferencial] = inputs[0].value.split('_')
+
+        if (generico === 'nombre' && (diferencial === 'pila' || diferencial === 'apellido')) {
+          if (diferencial === 'pila') {
+            nombre.nombrePila = inputs[1].value
+          } else {
+            nombre[diferencial] = inputs[1].value
+          }
+          usuario.nombre = nombre
+        } else if (generico === 'calle' && (diferencial === 'nombre' || diferencial === 'numero')) {
+          (calle as any)[diferencial] = inputs[1].value
+          usuario.calle = calle
+        } else if (generico === 'imagen' && (diferencial === 'grande' || diferencial === 'mediana' || diferencial === 'miniatura')) {
+          imagen[diferencial] = inputs[1].value
+          usuario.imagen = imagen
+        } else {
+          (usuario as any)[inputs[0].value] = inputs[1].value
+        }
+      })
+
+      return usuario
+    }
+  }
+
+  const manejadorFormulario = async (
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     event.preventDefault()
@@ -32,18 +93,36 @@ export function CrearExplorador ({ children }: Props): JSX.Element {
     if (!isElement || isElement === null) return
 
     try {
-      const respuestaServer = await fetch(
-        `http://localhost:4000/${endpoint.value}`,
-        {
-          method: opcion.toUpperCase()
-        }
-      ).then(async (res) => await res.json())
+      let respuestaServer
+      if (opcion.toUpperCase() === 'POST' || opcion.toUpperCase() === 'PATCH') {
+        const parametrosDiv = document.getElementById('parametros')
+        if (parametrosDiv === null) return
+        const divs = parametrosDiv.querySelectorAll('div')
+        const objeto: Anime | Usuario | undefined = comprobarParametros(divs, endpoint.value)
+        respuestaServer = await fetch(
+          `http://localhost:4002/${endpoint.value}`,
+          {
+            method: opcion.toUpperCase(),
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(objeto)
+          }
+        ).then(async (res) => await res.json())
+      } else {
+        respuestaServer = await fetch(
+          `http://localhost:4002/${endpoint.value}`,
+          {
+            method: opcion.toUpperCase()
+          }
+        ).then(async (res) => await res.json())
+      }
+
       if (respuestaServer.ok === false && respuestaServer.status === 404) {
         throw new Error('ERROR: Ese endpoint no existe')
       }
       setRespuesta(respuestaServer)
     } catch (error) {
-      console.error(error)
       if (error instanceof Error) {
         setRespuesta({ error: error.message })
       } else {
@@ -52,9 +131,13 @@ export function CrearExplorador ({ children }: Props): JSX.Element {
     }
   }
 
-  /* const manejadorParametros = (evento: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
-    setParametros([...parametros, { key: crypto.randomUUID() }])
-  } */
+  const manejadorCerrar = (evento: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    const key = evento.currentTarget.getAttribute('data-key')
+    const indiceEncontrado = parametros.findIndex((parametro) => { return parametro.key === key })
+    parametros.splice(indiceEncontrado, 1)
+    setParametros([...parametros])
+  }
+
   const manejadorParametros = (): void => {
     setParametros([...parametros, { key: crypto.randomUUID() }])
   }
@@ -62,58 +145,12 @@ export function CrearExplorador ({ children }: Props): JSX.Element {
   return (
     <>
       <article className='my-2 flex flex-col items-center gap-5 w-full md:bg-[rgb(var(--color-btn-border))] md:px-8 md:flex-row'>
-        <select
-          id='opciones'
-          className='text-black rounded-md p-1 min-w-max'
-          onChange={(e) => setOpcion(e.target.value)}
-        >
-          <option value='get'>GET</option>
-          <option value='post'>POST</option>
-          <option value='patch'>PATCH</option>
-          <option value='delete'>DELETE</option>
-        </select>
-        <header className='text-center bg-[rgb(var(--color-btn-border))] w-full py-3 rounded-sm'>
-          <form
-            className='flex flex-col gap-2 items-center text-black md:flex-row md:justify-between'
-            onSubmit={(event) => {
-              handleForm(event).catch((error) => console.error(error))
-            }}
-          >
-            <label className='flex flex-col sm:flex-row items-center gap-2'>
-              <span className='hidden md:block'>{children}</span>
-              http://localhost:4000/
-              <input
-                type='text'
-                name='endpoint'
-                className='rounded-md flex gap-2 w-full'
-              />
-            </label>
-            <input
-              type='submit'
-              value='Enviar'
-              className='bg-[#B3B2AE] w-max py-1 px-5 rounded-md cursor-pointer'
-            />
-          </form>
-        </header>
+        <SelectOpciones setOpcion={setOpcion} setParametros={setParametros} />
+        <FormularioExplorador manejadorFormulario={manejadorFormulario} flechaIzq={children} />
       </article>
       <article className='cuerpo-respuesta gap-2 text-black'>
-        <div className='bg-[rgb(var(--color-btn-border))] rounded p-2'>
-          {(!esGet && !esDetete) && (
-            <>
-              <button className='text-base text-slate-700' onClick={manejadorParametros}>Añadir parámetros</button>
-              <div id='parametros' className='flex flex-col gap-3'>
-                {parametros.map(({ key }) => {
-                  return (
-                    <div key={key} className='mx-2'>
-                      <input type='text' placeholder='Clave' className='w-28' />
-                      <input type='text' placeholder='Valor' />
-                      <hr />
-                    </div>
-                  )
-                })}
-              </div>
-            </>
-          )}
+        <div className='bg-[rgb(var(--color-btn-border))] rounded p-2 flex flex-col items-center overflow-y-auto'>
+          {(!esGet && !esDetete) && <AnadirParamentros manejadorParametros={manejadorParametros} manejadorCerrar={manejadorCerrar} parametros={parametros} />}
         </div>
 
         <code className='bg-[rgb(var(--color-btn-border))] w-full overflow-y-auto text-wrap rounded-sm'>
